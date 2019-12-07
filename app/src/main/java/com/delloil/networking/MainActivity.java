b0172fcc40e77ccaf47b216e4fd3f7820eba1f31
abhishekm76 <abhishek.amishra@gmail.com>
2019-12-07 22:44:09 +0530

    with JasonArray

--- app/src/main/java/com/delloil/networking/MainActivity.java
+++ app/src/main/java/com/delloil/networking/MainActivity.java
@@ -9,20 +9,24 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue requestQueue;
-    private String url;
+    private String url, urlarray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestQueue = Volley.newRequestQueue(this);
         url ="https://jsonplaceholder.typicode.com/todos/1";
+        urlarray ="https://jsonplaceholder.typicode.com/todos";
 
 
 
@@ -51,7 +55,34 @@ public class MainActivity extends AppCompatActivity {
                      public void onErrorResponse(VolleyError error) { }
         });
 
-        requestQueue.add(jsonObjectRequest);
+       requestQueue.add(jsonObjectRequest);
+
+            /*
+
+                public JsonArrayRequest(
+            int method,
+            String url,
+            @Nullable JSONArray jsonRequest,
+            Listener<JSONArray> listener,
+            @Nullable ErrorListener errorListener) {
+
+             */
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                urlarray,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("JSOSN","Array response "+ response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {  Log.d("JSOSN","Array error " );}
+                });
+
+        requestQueue.add(jsonArrayRequest);
 
     }
 
